584005c0f55e56457b5c12d1e63144c982983885
YrSk-tech <yuriy.syvak2002@gmail.com>
2024-10-09 17:44:30 +0300

    created and fully working hex to dec converter

---
+++ section_1/hex_to_dec/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    int number;
+    printf("write your hex number\n");
+    scanf("%X", &number);
+    printf("your number in dec: %d", number);
+    return 0;
+}

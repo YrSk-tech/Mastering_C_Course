5252e4df199af4172d0ee28532805369331b917c
YrSk-tech <yuriy.syvak2002@gmail.com>
2024-10-16 14:27:01 +0300

    created simple c app to check amount of students

---
+++ section_2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int girls = 12;
+	int boys = 9;
+	int students = girls + boys;
+
+	printf("in the clasroom, there are: \n");
+	printf("\t%d girls \n", girls);
+	printf("\t%d boys \n", boys);
+	printf("\ttotal amount of students: %d \n", students);
+
+
+
+	return 0;
+}

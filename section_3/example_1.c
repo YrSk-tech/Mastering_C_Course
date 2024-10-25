ddd3da329683f7d9153c13f61db28133c79504be
YrSk-tech <yuriy.syvak2002@gmail.com>
2024-10-25 13:35:03 +0300

    unsigned int example with long type

---
+++ section_3/example_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    unsigned students = 25U;
+    unsigned long long worldPopulation = 78621235945ULL;
+
+	printf("%12u | students in the class \n", students);
+    printf("%12llu | World population now \n", worldPopulation);
+
+	return 0;
+}
